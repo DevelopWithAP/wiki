0d3092edaa3e23e9c96c77dbceefac74998de6fa
Viktorija Bartasiute <viktorijabartasiute@Viktorijas-MacBook-Air.local>
2021-09-29 11:12:33 +0300

    Deleted forms.py

--- encyclopedia/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.forms import widgets
-
-class Search(forms.Form):
-    title = forms.CharField(max_length=25, widget=forms.TextInput(attrs={
-        "placeholder": "Search" 
-        }))
